3f469ea5b9ccfa60de3cd5ae046325f232e88513
emmanuel.eban <82966135+eebbann@users.noreply.github.com>
2025-03-17 21:16:13 +0100

    about

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -9,57 +9,31 @@ import { Timeline } from "@/components/ui/TimeLine";
 import { data } from "@/data/DataTimeline";
 import { getHero } from "@/sanity/queries/getHero";
 
-export default function About({ heroData }: { heroData: any }) {
-									const staticImages = [
-										{
-											src: image1,
-											alt: "Description of image 1",
-										},
-										{
-											src: image2,
-											alt: "Description of image 2",
-										},
-										{
-											src: image2,
-											alt: "Description of image 2",
-										},
-										{
-											src: image2,
-											alt: "Description of image 2",
-										},
-									];
+export default async function About() {
+  const heroData = await getHero("about"); // Fetching dynamically in a server component
 
-									return (
-										<div>
-											<Hero
-												isHomePage={true}
-												heading={heroData.hero.heading}
-												subheading={heroData.hero.subheading}
-												heroImage={heroData.hero.heroImage}
-												altText={heroData.hero.altText}
-											/>
-											{/* <Hero
-				isHomePage={false}
-				heading="Empowering students and Unlocking scientific potential"
-				subheading="I-Gene Life Science Foundation is a non-profit organization founded in august 2023"
-				heroImage={blood} // Now TypeScript knows this is a valid image type
-			/> */}
-											<Values
-												message=" Driving groundbreaking discoveries and scientific advancements."
-												display={false}
-											/>
-											<ImageGallery images={staticImages} />
-											<Timeline data={data} />
-										</div>
-									);
-								}
-export const getStaticProps = async () => {
-	const heroData = await getHero("about"); // Fetch Hero section for About Page
+  const staticImages = [
+    { src: image1, alt: "Description of image 1" },
+    { src: image2, alt: "Description of image 2" },
+    { src: image2, alt: "Description of image 2" },
+    { src: image2, alt: "Description of image 2" },
+  ];
 
-	return {
-		props: {
-			heroData,
-		},
-		revalidate: 60, // ISR: Refresh every 60 seconds
-	};
-};
+  return (
+    <div>
+      <Hero
+        isHomePage={true}
+        heading={heroData?.hero?.heading || "Default Heading"}
+        subheading={heroData?.hero?.subheading || "Default Subheading"}
+        heroImage={heroData?.hero?.heroImage || blood}
+        altText={heroData?.hero?.altText || "Hero Image"}
+      />
+      <Values
+        message="Driving groundbreaking discoveries and scientific advancements."
+        display={false}
+      />
+      <ImageGallery images={staticImages} />
+      <Timeline data={data} />
+    </div>
+  );
+}
