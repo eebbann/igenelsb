7d3f15f0fe24a8a4ec1e59ed3f9e88c8055fe59c
emmanuel.eban <82966135+eebbann@users.noreply.github.com>
2025-08-05 20:24:43 +0100

    updat

--- src/components/Values.tsx
+++ src/components/Values.tsx
@@ -9,14 +9,7 @@ export const Values = ({ message, display }: val) => {
 			>
 				{message}
 			</h1>
-			{display ?
-			(<button className="relative inline-flex mt-4  overflow-hidden rounded-lg p-[1.5px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
-				<span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#001e80_10%,#393BB2_70%,#E2CBFF_100%)]" />
-				<span className="inline-flex   w-full cursor-pointer items-center justify-center rounded-lg bg-slate-950 px-3 py-3 text-sm font-medium text-white backdrop-blur-3xl">
-					Join Us
-				</span>
-			</button>) :
-			 null }
+
 		</section>
 	);
 };
